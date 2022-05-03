031662c1b1bb46862487053397cd05f3218d72bf
kkj0419 <kkj0419@github.com>
2022-05-03 15:04:13 +0900

    초기 설정

---
+++ src/main/java/com/example/product_prgrms/ProductPrgrmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_prgrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPrgrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductPrgrmsApplication.class, args);
+    }
+
+}
