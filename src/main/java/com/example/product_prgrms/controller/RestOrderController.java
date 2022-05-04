9f3ddf7ddca9b2b19b19c9a306ebc2aa6833b834
kkj0419 <hgkimmme9@gmail.com>
2022-05-04 22:37:35 +0900

    RestController 추가

---
+++ src/main/java/com/example/product_prgrms/controller/RestOrderController.java
@@ -0,0 +1,47 @@
+package com.example.product_prgrms.controller;
+
+import com.example.product_prgrms.exception.OrderInsertException;
+import com.example.product_prgrms.model.order.OrderDTO;
+import com.example.product_prgrms.model.order.OrderListDTO;
+import com.example.product_prgrms.model.order.OrderRequest;
+import com.example.product_prgrms.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class RestOrderController {
+
+    private final OrderService orderService;
+
+    public RestOrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/orders")
+    public ResponseEntity<String> makeOrder(@RequestBody OrderRequest request) {
+        try {
+            orderService.createOrder(request);
+        }catch (OrderInsertException e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(new String("주문 생성"), HttpStatus.OK);
+    }
+
+    @GetMapping("/orders")
+    public List<OrderListDTO> getOrderList(){
+        return orderService.findAllOrders();
+    }
+
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable long id) {
+        try {
+            return new ResponseEntity<>(orderService.findOrderById(id), HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/example/product_prgrms/controller/RestProductController.java
@@ -0,0 +1,37 @@
+package com.example.product_prgrms.controller;
+
+import com.example.product_prgrms.model.product.ProductListDTO;
+import com.example.product_prgrms.model.product.ProductRequest;
+import com.example.product_prgrms.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@RestController()
+@RequestMapping("/api/v1")
+public class RestProductController {
+
+    private final ProductService productService;
+
+    public RestProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<ProductListDTO> getProductList(){
+        return productService.findAllProducts();
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<String> createProduct(@RequestBody ProductRequest request) {
+        try {
+            productService.createProduct(request);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(new String("생성 성공".getBytes(), StandardCharsets.UTF_8), HttpStatus.OK);
+    }
+}
